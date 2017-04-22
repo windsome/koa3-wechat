3f31248833745bbd5cf71de1a7702cb197ba15b9
windsome.feng <gw.feng@perspicace-china.com>
2017-04-23 00:06:51 +0800

    fix index.js, contain all modules

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-var wechat = require('./lib/wechat');
+var wechat = require('./wechat');
+var oauth = require('./oauth');
+var user = require('./user');
+var jssdk = require('./jssdk');
+var qrcode = require('./qrcode');
+var wepay = require('./wepay');
+var template = require('./template');
 
-module.exports = wechat;
+module.exports = {
+    wechat,
+    oauth,
+    user,
+    jssdk,
+    qrcode,
+    wepay,
+    template
+};
