db334ba9981a9cd61f5bf9dfab0adfa63cd0f86b
windsome <36081189@qq.com>
2017-07-01 22:42:44 +0800

    add index.js to src/. move compiled js to lib/.

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import wechat from './wechat';
+import oauth from './oauth';
+import user from './user';
+import jssdk from './jssdk';
+import qrcode from './qrcode';
+import wepay from './wepay';
+import template from './template';
+import media from './media';
+import menu from './menu';
+
+export default {
+    wechat,
+    oauth,
+    user,
+    jssdk,
+    qrcode,
+    wepay,
+    template,
+    media,
+    menu
+};
